d34d70d4f8dc4b0fbfd8d4e92ce29dba298f016d
Alexander Borodin <avbruu@gmail.com>
2020-03-13 20:53:39 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,50 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"io"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	builder := strings.Builder{}
+	reader := strings.NewReader(s)
+
+	prevRune, isPrevDigit := ' ', false
+	isFirst := true //Introduced to skip prevRune repeat in "4u" testcase (first rune is digit)
+
+	for r, _, err := reader.ReadRune(); err != io.EOF; r, _, err = reader.ReadRune() {
+		switch {
+		case r == '\\':
+			//Read,write escaped Rune
+			if r, _, err = reader.ReadRune(); err != io.EOF {
+				builder.WriteRune(r)
+				prevRune, isPrevDigit = r, false
+			}
+		case unicode.IsDigit(r):
+			//Two consecutive not escaped digits is error
+			if isPrevDigit {
+				return "", ErrInvalidString
+			}
+
+			//Repeat previous rune N-1 times, except if first rune in string is digit
+			n := int(r - '0')
+			if n > 1 && !isFirst {
+				builder.WriteString(strings.Repeat(string(prevRune), n-1)) //nolint:gomnd
+			}
+			prevRune, isPrevDigit = r, true
+		default:
+			builder.WriteRune(r)
+			prevRune, isPrevDigit = r, false
+		}
+
+		isFirst = false
+	}
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,9 +1,8 @@
 package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
-	"testing"
-
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 type test struct {
@@ -33,6 +32,10 @@ func TestUnpack(t *testing.T) {
 			err:      ErrInvalidString,
 		},
 		{
+			input:    "4u",
+			expected: "u",
+		},
+		{
 			input:    "",
 			expected: "",
 		},
@@ -44,7 +47,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackWithEscape(t *testing.T) {
-	t.Skip() // Remove if task with asterisk completed
+	//t.Skip() // Remove if task with asterisk completed
 
 	for _, tst := range [...]test{
 		{
@@ -62,6 +65,33 @@ func TestUnpackWithEscape(t *testing.T) {
 		{
 			input:    `qwe\\\3`,
 			expected: `qwe\3`,
+		},
+	} {
+		result, err := Unpack(tst.input)
+		require.Equal(t, tst.err, err)
+		require.Equal(t, tst.expected, result)
+	}
+}
+
+func TestUTF(t *testing.T) {
+	//t.Skip() // Remove if task with asterisk completed
+
+	for _, tst := range [...]test{
+		{
+			input:    `мама\4\5`,
+			expected: `мама45`,
+		},
+		{
+			input:    `мама\45`,
+			expected: `мама44444`,
+		},
+		{
+			input:    `мама\\я`,
+			expected: `мама\я`,
+		},
+		{
+			input:    `♪мама\\\я`,
+			expected: `♪мама\я`,
 		},
 	} {
 		result, err := Unpack(tst.input)
